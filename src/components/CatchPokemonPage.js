4dc6b5767cb815b899054315262da976983d0d85
yanrivera19 <yan.rivera.6@gmail.com>
2022-05-02 22:28:53 -0700

    Added Loader component. If data takes time to load for the first time on the catch a pokemon page, the Loader will be displayed. Fixed issue with fonts in index.html file.

--- src/components/CatchPokemonPage.js
+++ src/components/CatchPokemonPage.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import {
     fetchPokemons,
@@ -6,17 +6,20 @@ import {
 } from "../features/pokemon/pokemonSlice";
 import { useDispatch, useSelector } from "react-redux";
 import PokemonItem from "./PokemonItem";
+import Loader from "./Loader";
 
 const CatchPokemonPage = () => {
     const dispatch = useDispatch();
     const { pokemonItems, catchedPokemons } = useSelector(
         (state) => state.pokemon
     );
+    const [isLoading, setIsLoading] = useState(true);
     const randomNum = () => Math.floor(Math.random() * 20) * 10;
 
     useEffect(() => {
         dispatch(fetchPokemons(randomNum()));
         dispatch(getCatchedPokemons());
+        setIsLoading(false);
     }, [dispatch]);
 
     const renderList = pokemonItems.map((pokemon) => (
@@ -29,34 +32,40 @@ const CatchPokemonPage = () => {
 
     return (
         <div>
-            <Link to="/" className="home-link">
-                <div className="d-flex justify-content-start align-items-center">
-                    <button className="btn p-2 ">
-                        <i className="fa-solid fa-arrow-left go-back-btn p-2 rounded-circle"></i>
-                    </button>
-                    <p className="mb-0 ">Go back</p>
-                </div>
-            </Link>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <>
+                    <Link to="/" className="home-link">
+                        <div className="d-flex justify-content-start align-items-center">
+                            <button className="btn p-2 ">
+                                <i className="fa-solid fa-arrow-left go-back-btn p-2 rounded-circle"></i>
+                            </button>
+                            <p className="mb-0 ">Go back</p>
+                        </div>
+                    </Link>
 
-            <button
-                className="btn fs-5 catch-pok-btn home-catch-btn mt-1 mb-4"
-                onClick={() => dispatch(fetchPokemons(randomNum()))}
-            >
-                See More Pokémons!
-            </button>
-            <header className="pok-header mb-4 mt-2">
-                <h3 className="fw-bold">
-                    <i className="fa-solid fa-circle-dot me-2" />
-                    Catch 'em all!
-                    <i className="fa-solid fa-circle-dot ms-2" />
-                </h3>
-            </header>
-            <div
-                data-testid="pok-list"
-                className="row g-2 justify-content-center m-auto pok-list"
-            >
-                {renderList}
-            </div>
+                    <button
+                        className="btn fs-5 catch-pok-btn home-catch-btn mt-1 mb-4"
+                        onClick={() => dispatch(fetchPokemons(randomNum()))}
+                    >
+                        See More Pokémons!
+                    </button>
+                    <header className="pok-header mb-4 mt-2">
+                        <h3 className="fw-bold">
+                            <i className="fa-solid fa-circle-dot me-2" />
+                            Catch 'em all!
+                            <i className="fa-solid fa-circle-dot ms-2" />
+                        </h3>
+                    </header>
+                    <div
+                        data-testid="pok-list"
+                        className="row g-2 justify-content-center m-auto pok-list"
+                    >
+                        {renderList}
+                    </div>
+                </>
+            )}
         </div>
     );
 };
---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+const Loader = () => {
+    return (
+        <div
+            className="d-flex justify-content-center align-items-center"
+            style={{ marginTop: "250px" }}
+        >
+            <div className="spinner-grow" role="status"></div>
+            <div className="spinner-grow" role="status"></div>
+            <div className="spinner-grow" role="status"></div>
+        </div>
+    );
+};
+
+export default Loader;
