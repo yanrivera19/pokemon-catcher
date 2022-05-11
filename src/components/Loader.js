4c9b34e6e560eb86d7aff374979bc240ecf08a36
yanrivera19 <yan.rivera.6@gmail.com>
2022-05-11 07:40:34 -0700

    Removed marginTop style

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ const Loader = () => {
     return (
         <div
             className="d-flex justify-content-center align-items-center"
-            style={{ marginTop: "250px" }}
+            style={{ color: "white" }}
         >
             <div className="spinner-grow" role="status"></div>
             <div className="spinner-grow" role="status"></div>
