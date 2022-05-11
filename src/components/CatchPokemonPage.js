56f3e67bf9e26278fba2445a512d0a3e0cd92def
yanrivera19 <yan.rivera.6@gmail.com>
2022-05-11 07:37:52 -0700

    Added animations to the list of pokemons using framer-motion library, also added a set timeout to delay the displaying of the pokemon list for half a second. I did this because when navigating to this page, initially you could see in some instances the previously displayed pokemons for a second. Now you can just see the new pokemons after half a second.

--- src/components/CatchPokemonPage.js
+++ src/components/CatchPokemonPage.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import {
     fetchPokemons,
@@ -7,19 +7,20 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import PokemonItem from "./PokemonItem";
 import Loader from "./Loader";
+import { motion, AnimatePresence } from "framer-motion";
 
 const CatchPokemonPage = () => {
     const dispatch = useDispatch();
     const { pokemonItems, catchedPokemons } = useSelector(
         (state) => state.pokemon
     );
-    const [isLoading, setIsLoading] = useState(true);
+    const [hide, setHide] = useState(true);
     const randomNum = () => Math.floor(Math.random() * 20) * 10;
 
     useEffect(() => {
         dispatch(fetchPokemons(randomNum()));
         dispatch(getCatchedPokemons());
-        setIsLoading(false);
+        setTimeout(() => setHide(false), 500);
     }, [dispatch]);
 
     const renderList = pokemonItems.map((pokemon) => (
@@ -32,39 +33,46 @@ const CatchPokemonPage = () => {
 
     return (
         <div>
-            {isLoading ? (
-                <Loader />
-            ) : (
-                <>
-                    <Link to="/" className="home-link">
-                        <div className="d-flex justify-content-start align-items-center">
-                            <button className="btn p-2 ">
-                                <i className="fa-solid fa-arrow-left go-back-btn p-2 rounded-circle"></i>
-                            </button>
-                            <p className="mb-0 fw-bold">Go back</p>
-                        </div>
-                    </Link>
-
-                    <button
-                        className="btn fs-5 see-more-btn mt-1 mb-4"
-                        onClick={() => dispatch(fetchPokemons(randomNum()))}
-                    >
-                        See More Pokémons!
+            <Link to="/" className="home-link">
+                <div className="d-flex justify-content-start align-items-center">
+                    <button className="btn p-2 ">
+                        <i className="fa-solid fa-arrow-left go-back-btn p-2 rounded-circle"></i>
                     </button>
-                    <header className="pok-header mb-4 mt-2">
-                        <h3 className="fw-bold">
-                            <i className="fa-solid fa-circle-dot me-2" />
-                            Catch 'em all!
-                            <i className="fa-solid fa-circle-dot ms-2" />
-                        </h3>
-                    </header>
-                    <div
-                        data-testid="pok-list"
-                        className="row g-2 justify-content-center m-auto pok-list"
+                    <p className="mb-0 go-back-txt">Go back</p>
+                </div>
+            </Link>
+
+            <button
+                className="btn fs-5 see-more-btn mt-1 mb-4"
+                onClick={() => dispatch(fetchPokemons(randomNum()))}
+            >
+                See More Pokémons!
+            </button>
+            <header className="pok-header mb-4 mt-2">
+                <h3 className="fw-bold">
+                    <i className="fa-solid fa-circle-dot me-2" />
+                    Catch 'em all!
+                    <i className="fa-solid fa-circle-dot ms-2" />
+                </h3>
+            </header>
+            {hide ? (
+                <div style={{ marginTop: "100px" }}>
+                    <Loader />
+                </div>
+            ) : (
+                <AnimatePresence>
+                    <motion.div
+                        initial={{ opacity: 0, scale: 0.75 }}
+                        animate={{ opacity: 1, scale: 1 }}
                     >
-                        {renderList}
-                    </div>
-                </>
+                        <div
+                            data-testid="pok-list"
+                            className="row g-2 justify-content-center m-auto pok-list"
+                        >
+                            {renderList}
+                        </div>
+                    </motion.div>
+                </AnimatePresence>
             )}
         </div>
     );
